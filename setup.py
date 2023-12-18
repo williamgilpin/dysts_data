311b9316833e380199a6311ca1add5f1ed10721b
williamgilpin <williamgilpin@gmail.com>
2023-12-18 16:05:18 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'dysts_data',
+      packages=['dysts_data'],
+      version='0.1',
+      install_requires = ["numpy", "scipy", "pandas"],
+      # package_dir={'dysts_data': 'dysts'},
+      package_data={'dysts_data': ['data/*', 'benchmarks/*']},
+     )
