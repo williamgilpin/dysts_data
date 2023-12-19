8cb668142ddd2fd6eebe4f3ae84db2e55030bb95
williamgilpin <williamgilpin@gmail.com>
2023-12-19 15:26:59 -0800

    latest

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name = 'dysts_data',
       packages=['dysts_data'],
       version='0.1',
-      install_requires = ["numpy", "scipy", "pandas"],
+      install_requires = [],
       package_dir={'dysts_data': 'dysts_data'},
       package_data={'dysts_data': ['data/*', 'benchmarks/*']},
      )
