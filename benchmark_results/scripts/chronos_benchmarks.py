9a0dceac6c2b54c970d8e6259300226d5e560347
williamgilpin <williamgilpin@gmail.com>
2024-08-05 16:53:51 -0500

    latest results

--- benchmark_results/scripts/chronos_benchmarks.py
+++ benchmark_results/scripts/chronos_benchmarks.py
@@ -8,8 +8,8 @@ import argparse
 # Function to process command-line arguments
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Process a command-line argument.')
-    parser.add_argument('arg1', type=str, help='The name of the system to be processed')
-    parser.add_argument('arg2', type=str, nargs='?', default='30', help='An optional integer argument for pts_per_period')
+    parser.add_argument('arg1', type=str, help='The name of the dynamical system to be processed')
+    parser.add_argument('arg2', type=int, nargs='?', const=30, default=30, help='An optional integer argument for pts_per_period')
     args = parser.parse_args()
     return args
 args = parse_arguments()
@@ -55,6 +55,7 @@ integrator_args = {
 ic_traj = eq.make_trajectory(1000, pts_per_period=10, atol=1e-12, rtol=1e-12)
 np.random.seed(0)
 sel_inds = np.random.choice(range(1000), size=num_ic, replace=False).astype(int)
+# sel_inds = np.linspace(0, 1000, 20).astype(int)
 ic_context_test = ic_traj[sel_inds, :]
 traj_test = list()
 for ic in ic_context_test:
@@ -68,7 +69,7 @@ traj_train = traj_test[:, :training_length] ## Training data for standard models
 traj_test_context = traj_test[:, training_length - context_length:training_length]
 ic_test = traj_test_context[:, -1] # Last seen point                                                                 
 traj_test_forecast = traj_test[:, training_length:]
-save_str_reference = f"forecast_{eq.name}_true_chronos"
+save_str_reference = f"forecast_{eq.name}_granularity{pts_per_period}_true_chronos"
 save_str_reference = os.path.join(dirname, save_str_reference)
 np.save(save_str_reference, traj_test_forecast, allow_pickle=True)
 
@@ -76,9 +77,17 @@ from models import ChronosModel
 
 ## Run chronos zero-shot benchmark
 for model_size in ["tiny", "mini", "small", "base", "large"]:
+# for model_size in ["large"]:
     model = ChronosModel(model=model_size, context=context_length, n_samples=n_average, 
                                         prediction_length=forecast_length)
     print(model_size, flush=True)
+    
+    save_str = f"forecast_{eq.name}_{model_size}_granularity{pts_per_period}"
+    save_str = os.path.join(dirname, save_str)
+    if os.path.exists(save_str):
+        print("Skipping, already found: " + save_str, flush=True)
+        continue
+    
     all_traj_forecasts = list()
     ## Loop over the replicate trajectories
     for itr, traj in enumerate(traj_test_context):
@@ -86,9 +95,7 @@ for model_size in ["tiny", "mini", "small", "base", "large"]:
         forecast_multivariate = np.array(model.predict(traj.T))
         all_traj_forecasts.append(forecast_multivariate.copy())
 
-    all_traj_forecasts = np.array(all_traj_forecasts)
-    all_traj_forecasts = np.moveaxis(all_traj_forecasts, (1, 3, 2), (2, 1, 3))
+        all_traj_forecasts2 = np.array(all_traj_forecasts)
+        all_traj_forecasts2 = np.moveaxis(all_traj_forecasts2, (1, 3, 2), (2, 1, 3))
 
-    save_str = f"forecast_{eq.name}_{model.name}"
-    save_str = os.path.join(dirname, save_str)
-    np.save(save_str, all_traj_forecasts, allow_pickle=True)
+        np.save(save_str, all_traj_forecasts2, allow_pickle=True)
--- benchmark_results/scripts/darts_benchmarks.py
+++ benchmark_results/scripts/darts_benchmarks.py
@@ -15,19 +15,13 @@ args = parse_arguments()
 equation_name = args.arg1
 print(equation_name, flush=True)
 
-
-num_ic = 20*2
+num_ic = 20
 training_length = 512
-context_length = 512
-# forecast_length = 400 # Maximum for these models
-forecast_length = 300 # Maximum for these models
-# n_average = 20
-n_average = 1
-# pts_per_period = 40
-# pts_per_period = 30
-pts_per_period = 25
+context_length = 512 # Not used
+forecast_length = 300 # Maximum horizon
+pts_per_period = 30
 
-dirname = f"darts_benchmarks_context_{context_length}_granularity_{pts_per_period}"
+dirname = f"darts_benchmarks_granularity_{pts_per_period}"
 ## Check if the directory exists, if not, create it
 import os
 if not os.path.exists(dirname):
@@ -48,6 +42,7 @@ integrator_args = {
 ic_traj = eq.make_trajectory(1000, pts_per_period=10, atol=1e-12, rtol=1e-12)
 np.random.seed(0)
 sel_inds = np.random.choice(range(1000), size=num_ic, replace=False).astype(int)
+# sel_inds = np.linspace(0, 1000, 20).astype(int)
 ic_context_test = ic_traj[sel_inds, :]
 traj_test = list()
 for ic in ic_context_test:
@@ -61,7 +56,7 @@ traj_train = traj_test[:, :training_length] ## Training data for standard models
 traj_test_context = traj_test[:, training_length - context_length:training_length]
 ic_test = traj_test_context[:, -1] # Last seen point                                                                 
 traj_test_forecast = traj_test[:, training_length:]
-save_str_reference = f"forecast_{eq.name}_true_dysts"
+save_str_reference = f"forecast_{eq.name}_granularity_{pts_per_period}_true_dysts"
 save_str_reference = os.path.join(dirname, save_str_reference)
 np.save(save_str_reference, traj_test_forecast, allow_pickle=True)
 
@@ -96,57 +91,59 @@ else:
 #         "devices": n,
          #    "gpus": [0],  # use "devices" instead of "gpus" for PyTorch Lightning >= 1.7.                                    #    "auto_select_gpu": True,                                                                                
     }
-pl_trainer_kwargs = [gpu_params]
+pl_trainer_kwargs = [gpu_params] # global
 model_static_dict = {"pl_trainer_kwargs" : pl_trainer_kwargs}
 
 
-def import_model_by_name(model_name, *args):
+def add_gpu_kwargs(kw):
+    """Given a set of keyword arguments for a GPU run, update the arguments to exploit the GPU.
+    Requires global arguments gpu_params and has_gpu"""
+    if has_gpu:
+        try:
+            kw.update({"pl_trainer_kwargs": gpu_params})
+        except Exception as e:
+            print(f"Failed to initialize with GPU parameters: {e}", flush=True)
+    return kw
+
+def import_model_by_name(model_name, *args, **kwargs):
     if model_name == "NBEATS":
         from darts.models.forecasting.nbeats import NBEATSModel
-        return NBEATSModel
+        return NBEATSModel(*args, **add_gpu_kwargs(kwargs))
     elif model_name == "Transformer":
         from darts.models.forecasting.transformer_model import TransformerModel
-        return TransformerModel
+        return TransformerModel(*args, **add_gpu_kwargs(kwargs))
     elif model_name == "LSTM":
         from darts.models.forecasting.rnn_model import RNNModel
-        return RNNModel
+        return RNNModel(*args, **add_gpu_kwargs(kwargs))
     elif model_name == "TiDE":
         from darts.models.forecasting.tide_model import TiDEModel
-        return TiDEModel
+        return TiDEModel(*args, **add_gpu_kwargs(kwargs))
     elif model_name == "Linear":
         from darts.models.forecasting.linear_regression_model import LinearRegressionModel
-        return LinearRegressionModel
-    # elif model_name == "NVAR":
-    #     from esn import NVARForecast
-    #     nvar_curry = lambda x: NVARForecast(x, delay=context_length)
-    #     return NVARForecast
+        return LinearRegressionModel(*args, **kwargs)
+    elif model_name == "NVAR":
+        from esn import NVARForecast
+        return NVARForecast(*args, **kwargs)
     else:
         raise ValueError(f"Unknown model name: {model_name}")
         
 def run_forecast_benchmark(model_name, traj_train, all_hyperparameters, forecast_length, gpu_params, equation_name, dirname=None):
-    ModelClass = import_model_by_name(model_name)
+
     all_pred = list()
     
     for i in range(len(traj_train)):
         print(i, flush=True)
         train_data = np.copy(traj_train)[i]
-        y_train_ts = TimeSeries.from_values(train_data)
-        
         try:
-            pc = dict()
-            pc.update(all_hyperparameters)
-            pc.update({"pl_trainer_kwargs": gpu_params})
-            model = ModelClass(**pc)
-        except Exception as e:
-            print(f"Failed to initialize with GPU parameters: {e}", flush=True)
-            model = ModelClass(**all_hyperparameters)
-        
-        # Fit the forecasting model on the given data
-        model.fit(y_train_ts)
-        
-        # Attempt to predict the validation data
-        try:
-            y_val_pred = model.predict(forecast_length).values().squeeze()
+            if model_name == "NVAR":
+                model = import_model_by_name(model_name, traj_train.shape[-1], **all_hyperparameters)
+                model.fit(np.copy(train_data))
+                y_val_pred = model.predict(forecast_length).squeeze().copy()
+            else:
+                y_train_ts = TimeSeries.from_values(train_data.copy())
+                model = import_model_by_name(model_name, **all_hyperparameters)
+                model.fit(y_train_ts)
+                y_val_pred = model.predict(forecast_length).values().squeeze()
         except Exception as e:
             print(f"Failed to predict {equation_name} {model_name}: {e}", flush=True)
             y_val_pred = np.array([None] * forecast_length)
@@ -155,15 +152,30 @@ def run_forecast_benchmark(model_name, traj_train, all_hyperparameters, forecast
         all_pred.append(np.copy(y_val_pred))
     
     all_pred = np.array(all_pred)
-    save_str = f"forecast_{equation_name}_{model_name}"
+    save_str = f"forecast_{equation_name}_{model_name}_granularity{pts_per_period}"
     save_str = os.path.join(dirname, save_str)
     np.save(save_str, all_pred, allow_pickle=True)
 
+    
+def smape(y, yhat):
+    """Symmetric mean absolute percentage error.
+
+    Args:
+        y (np.ndarray): An array containing the true values.
+        yhat (np.ndarray): An array containing the predicted values.
+
+    Returns:
+        (float): the scalar sMAPE score
+    """
+    assert len(yhat) == len(y)
+    n = len(y)
+    err = np.abs(y - yhat) / (np.abs(y) + np.abs(yhat)) * 100
+    return (2 / n) * np.sum(err)
+
 import itertools
 from darts import TimeSeries
-from sklearn.model_selection import TimeSeriesSplit
-def cross_validate_hyperparameters(model_name, traj_train, hyperparams, hyperparameter_candidates, forecast_length=20, n_splits=5):
-    ModelClass = import_model_by_name(model_name)
+# from sklearn.model_selection import TimeSeriesSplit
+def cross_validate_hyperparameters(model_name, traj_train, hyperparams, hyperparameter_candidates, gpu_params, forecast_length=20, n_splits=5):
     all_scores = list()
     ## Create a stucture with all possible combinations of suggested hyperparameters
     all_combinations = list(itertools.product(*hyperparameter_candidates.values()))
@@ -172,117 +184,107 @@ def cross_validate_hyperparameters(model_name, traj_train, hyperparams, hyperpar
     for combination in all_combinations:
         ## Make the hyperparameters dictionary for a particular combination
         hyperparams_combination = dict(zip(keys, combination))
-        scores = list()
-        tscv = TimeSeriesSplit(n_splits=n_splits)
         
-        ## Cross-validate the model
-        for train_index, test_index in tscv.split(traj_train):
-            train_data, test_data = traj_train[train_index], traj_train[test_index]
-            y_train_ts = TimeSeries.from_values(train_data)
-            y_test_ts = TimeSeries.from_values(test_data)
+        split_index = int(0.85 * traj_train.shape[1])
+        scores = list()
+        for i in range(traj_train.shape[0]): # Iterate over all trajectories
+            train_data, test_data = traj_train[i, :split_index].copy(), traj_train[i, split_index:].copy()
+            print(train_data.shape, test_data.shape, traj_train.shape)
             
             pc = dict()
             pc.update(hyperparams)
             pc.update(hyperparams_combination)
             
             try:
-                model = ModelClass(**pc)
-                model.fit(y_train_ts)
-                y_pred = model.predict(len(test_data)).values().squeeze()
-                y_true = y_test_ts.values().squeeze()
                 
-                score = np.mean((y_pred - y_true) ** 2)  # Example scoring metric: MSE
+                if model_name == "NVAR":
+                    model = import_model_by_name(model_name, train_data.shape[-1], **pc)
+                    model.fit(np.copy(train_data))
+                    y_pred = model.predict(len(test_data)).squeeze().copy()
+                else:
+                    y_train_ts = TimeSeries.from_values(np.copy(train_data))
+                    
+                    model = import_model_by_name(model_name, **pc)
+                    model.fit(y_train_ts)
+                    y_pred = model.predict(len(test_data)).values().squeeze().copy()
+                
+                y_test_ts = TimeSeries.from_values(np.copy(test_data))
+                y_true = y_test_ts.values().squeeze().copy()
+                score = smape(y_pred, y_true)  # Example scoring metric: MSE
                 scores.append(score)
                 
             except Exception as e:
                 print(f"Failed to cross-validate with parameters {hyperparams_combination}: {e}", flush=True)
                 scores.append(np.nan)
+
         
-        mean_score = np.mean(scores)
-        all_scores.append((mean_score, hyperparams_combination))
-    
+        all_scores.append((np.nanmean(scores), hyperparams_combination))
+       
+    print(all_scores)
     best_score, best_params = min(all_scores, key=lambda x: x[0])
     best_params.update(hyperparams)
-    print(f"Best hyperparameters: {best_params} with score: {best_score}", flush=True)
+    print(f"Best hyperparameters for {model_name}: {best_params} with score: {best_score}", flush=True)
     
     return best_params
-    
 
-model_name = "Transformer"
+model_name = "NVAR"
 print(model_name, flush=True)
-context_length = 100
-hyperparameters = {"input_chunk_length": context_length, "output_chunk_length": 1}
-hyperparameter_candidates = {"input_chunk_length": [5, 25, 50, 75, 100]}
-hyperparameters = cross_validate_hyperparameters(model_name, traj_train, hyperparameters, hyperparameter_candidates)
+hyperparameters = {}
+hyperparameter_candidates = {"delay": [5, 25, 50, 75, 100]}
+hyperparameters = cross_validate_hyperparameters(model_name, traj_train, hyperparameters, hyperparameter_candidates, gpu_params)
 run_forecast_benchmark(
-    model_name, traj_train, hyperparameters, context_length, 
-    forecast_length, gpu_params, equation_name, dirname=dirname
-)
-
-model_name = "NBEATS"
-print(model_name, flush=True)
-context_length = 100
-hyperparameters = {"input_chunk_length": context_length, "output_chunk_length": 1}
-hyperparameter_candidates = {"input_chunk_length": [5, 25, 50, 75, 100]}
-hyperparameters = cross_validate_hyperparameters(model_name, traj_train, hyperparameters, hyperparameter_candidates)
-run_forecast_benchmark(
-    model_name, traj_train, hyperparameters, context_length, 
+    model_name, traj_train, hyperparameters,
     forecast_length, gpu_params, equation_name, dirname=dirname
 )
 
 model_name = "LSTM"
 print(model_name, flush=True)
-context_length = 100
-hyperparameters = {"input_chunk_length": context_length, "output_chunk_length": 1}
-hyperparameter_candidates = {"input_chunk_length": [5, 25, 50, 75, 100]}
-hyperparameters = cross_validate_hyperparameters(model_name, traj_train, hyperparameters, hyperparameter_candidates)
+hyperparameters = {}
+hyperparameter_candidates = {"input_chunk_length": [1, 10, 25], "training_length": [51, 101, 256]}
+hyperparameters = cross_validate_hyperparameters(model_name, traj_train, hyperparameters, hyperparameter_candidates, gpu_params)
 run_forecast_benchmark(
-    model_name, traj_train, hyperparameters, context_length, 
+    model_name, traj_train, hyperparameters,
+    forecast_length, gpu_params, equation_name, dirname=dirname
+)
+
+model_name = "Linear"
+print(model_name, flush=True)
+hyperparameters = {}
+hyperparameter_candidates = {"lags": [5, 25, 50, 75, 100]}
+hyperparameters = cross_validate_hyperparameters(model_name, traj_train, hyperparameters, hyperparameter_candidates, gpu_params)
+run_forecast_benchmark(
+    model_name, traj_train, hyperparameters, 
     forecast_length, gpu_params, equation_name, dirname=dirname
 )
 
 model_name = "TiDE"
 print(model_name, flush=True)
-context_length = 100
-hyperparameters = {"input_chunk_length": context_length, "output_chunk_length": 1}
+hyperparameters = {"output_chunk_length": 1}
 hyperparameter_candidates = {"input_chunk_length": [5, 25, 50, 75, 100]}
-hyperparameters = cross_validate_hyperparameters(model_name, traj_train, hyperparameters, hyperparameter_candidates)
+hyperparameters = cross_validate_hyperparameters(model_name, traj_train, hyperparameters, hyperparameter_candidates, gpu_params)
 run_forecast_benchmark(
-    model_name, traj_train, hyperparameters, context_length, 
+    model_name, traj_train, hyperparameters, 
     forecast_length, gpu_params, equation_name, dirname=dirname
 )
 
-
-model_name = "Linear"
+model_name = "Transformer"
 print(model_name, flush=True)
-context_length = 100
-hyperparameters = {"lags": context_length}
-hyperparameter_candidates = {"lags": [5, 25, 50, 75, 100]}
-hyperparameters = cross_validate_hyperparameters(model_name, traj_train, hyperparameters, hyperparameter_candidates)
+hyperparameters = {"output_chunk_length": 1}
+hyperparameter_candidates = {"input_chunk_length": [5, 25, 50, 75, 100][::-1]}
+hyperparameters = cross_validate_hyperparameters(model_name, traj_train, hyperparameters, hyperparameter_candidates, gpu_params)
 run_forecast_benchmark(
-    model_name, traj_train, hyperparameters, context_length, 
+    model_name, traj_train, hyperparameters, 
     forecast_length, gpu_params, equation_name, dirname=dirname
 )
 
-from esn import NVARForecast
-print("NVAR", flush=True)
-# hyperparameters = {"delay": context_length}
-hyperparameters = {"delay": 100}
-hyperparameter_candidates = {"delay": [5, 25, 50, 75, 100]}
-all_pred = list()
-for i in range(len(traj_test_context)):
-    print(i, flush=True)
-    train_data = np.copy(traj_train)[i]
-    try:
-        model = NVARForecast(train_data.shape[-1], delay=context_length)
-        model.fit(train_data)
-        y_val_pred = model.predict(forecast_length)
-    except AssertionError:
-        print("Integration error encountered, skipping this entry for now")
-        y_val_pred = np.array([None] * forecast_length)
-    all_pred.append(np.copy(y_val_pred.squeeze()))
-all_pred = np.array(all_pred)
-save_str = f"forecast_{eq.name}_NVAR"
-save_str = os.path.join(dirname, save_str)
-np.save(save_str, all_pred, allow_pickle=True)
+model_name = "NBEATS"
+print(model_name, flush=True)
+hyperparameters = {"output_chunk_length": 1}
+hyperparameter_candidates = {"input_chunk_length": [5, 25, 50, 75, 100]}
+hyperparameters = cross_validate_hyperparameters(model_name, traj_train, hyperparameters, hyperparameter_candidates, gpu_params)
+run_forecast_benchmark(
+    model_name, traj_train, hyperparameters, 
+    forecast_length, gpu_params, equation_name, dirname=dirname
+)
+
 
